f25a391b7d6943645414fac4b12fb904ba86492d
BricoBC <barajas.c.bruno.n@gmail.com>
2023-04-13 22:57:09 -0500

    Exercise 1 to 20

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LearnPy
 Los códigos a continuación són proporcionados de siguiente libro: https://drive.google.com/file/d/1iVR_Hjg50NI82DFjqnQ_WFI8D1KJKLJl/view?usp=share_link 
+
+La ejecución de los programas son de la siguiente forma:
+1.- Reconoce el nombre del archivo.
+2.- En la terminal escribe python <nombre_archivo>.py
---
+++ exercise2.py
@@ -0,0 +1,9 @@
+# Un comentario, esto es para que puedas leer tu programa más tarde.
+# Python ignora cualquier cosa después del signo de #
+
+print("I could have code like this.") # y el comentario posterior es ignorado
+
+# También puede usar un comentario para "deshabilitar" o comentar el código:
+# print("This won't run.")
+
+print("This will run.")
